f8b6b5d9ac64842b754255fbf5f3ab6c5c86e6b4
kermenz-kas <keremenzkas@gmail.com>
2023-04-27 02:48:36 -0700

    hare bug fix_2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,12 @@
-#include "lists.h"
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
 
 /**
- * hare - Prints a string before the main function is executed.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void hare(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
